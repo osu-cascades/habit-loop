3144dde2c497c5563d2e723f757274c77f46ce2a
Nathan <hildebrn@oregonstate.edu>
2018-11-04 11:40:02 -0800

    Update app.js to display simple login screen

--- App.js
+++ App.js
@@ -1,62 +1,67 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+    return (
+      <View style={styles.container}>
+      <Text style={styles.heading}>CBT Training Loop</Text>
+      <Text style={styles.paragraph}>[Insert Logo here]</Text>
+      <View style={styles.login}>
+      <Text style={styles.login.text}>
+      Username:
+      </Text>
+      <Text style={styles.login.text}>
+      Password:
+      </Text>
+      </View>
+        <Card>
+        </Card>
+      </View>
+    );
   }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  heading: {
+    fontSize: '30px',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: '#ecf0f1',
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  login: {
+    flex: 1,
+    width: '200px',
+    padding:'100px',
+    border: '1px solid black',
+    textAlign:'left',
+    text: {
+      width:'50%',
+      textAlign: 'left',
+      fontSize: '20px',
+    }
+  }
 });
