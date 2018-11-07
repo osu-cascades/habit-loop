2390001026138726777eed555fbfe85f0fe280e1
DJ Shamblin <hamblindj@gmail.com>
2018-11-07 09:44:08 -0800

    add apollo provider

--- Main.js
+++ Main.js
@@ -1,10 +1,20 @@
 import Expo from 'expo';
 import React from 'react';
 import RootApp from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000"
+})
 
 class App extends React.Component {
   render() {
-    return <RootApp />;
+    return (
+        <ApolloProvider client={client}>
+            <RootApp />
+        </ApolloProvider>
+    );
   }
 }
 
