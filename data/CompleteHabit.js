8bd1ec08ed490ece68b8f0e7621a7c644d756479
donaldjr <hamblindj@gmail.com>
2019-04-28 11:48:15 -0700

    remove user_id and username since those are in the jwt token

--- data/CompleteHabit.js
+++ data/CompleteHabit.js
@@ -2,8 +2,8 @@ import { graphql } from 'react-apollo';
 import gql from "graphql-tag";
 
 const COMPLETE_HABIT = gql`
-    mutation completeHabit($user_id: String!, $habit_id: String!, $recurrence: String!){
-        completeHabit(user_id: $user_id, habit_id: $habit_id, recurrence: $recurrence)
+    mutation completeHabit($habit_id: String!, $recurrence: String!){
+        completeHabit(habit_id: $habit_id, recurrence: $recurrence)
     }
 `;
 
