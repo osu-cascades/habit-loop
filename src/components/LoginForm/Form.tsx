bb0584924ec2377cbe3b6c107acafc362079a565
Johnny Phelan <phelanjo@oregonstate.edu>
2019-10-28 12:05:04 -0700

    Move styled components to own file, clean up imports in Login form.

--- src/components/LoginForm/Form.tsx
+++ src/components/LoginForm/Form.tsx
@@ -1,42 +1,7 @@
 import React from 'react';
-import {
-    Text,
-    View,
-} from 'react-native';
 import { SignupButton } from '../';
 import { ButtonText } from '../basic';
-import styled from 'styled-components/native';
-
-export const FormLabel = styled.Text`
-    font-family: Avenir Next;
-    color: #000;
-    opacity: 0.8;
-`;
-
-export const LoginButton = styled.TouchableOpacity`
-    background-color: #666;
-    padding-vertical: 10;
-    width: 200;
-    margin: 0 auto;
-    margin-top: 10;
-    border-radius: 5;
-`;
-
-export const LoginView = styled.View`
-    padding: 20px;
-    border-radius: 5;
-    background-color: #E6B43C;
-`;
-
-export const LoginInput = styled.TextInput`
-    height: 40;
-    background-color: rgba(255, 255, 255, 0.9);
-    margin-bottom: 10;
-    padding-horizontal: 10;
-    font-family: Avenir Next;
-    border: ${props => (props.error ? '1px solid tomato' : '1px solid #999999')}
-    border-radius: 4px;
-`;
+import { FormLabel, LoginButton, LoginInput, LoginView } from './login_style'
 
 export default LoginForm = props => (
     <LoginView>
---
+++ src/components/LoginForm/login_style.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native'
+
+export const FormLabel = styled.Text`
+    font-family: Avenir Next;
+    color: #000;
+    opacity: 0.8;
+`;
+
+export const LoginButton = styled.TouchableOpacity`
+    background-color: #666;
+    padding-vertical: 10;
+    width: 200;
+    margin: 0 auto;
+    margin-top: 10;
+    border-radius: 5;
+`;
+
+export const LoginView = styled.View`
+    padding: 20px;
+    border-radius: 5;
+    background-color: #E6B43C;
+`;
+
+export const LoginInput = styled.TextInput`
+    height: 40;
+    background-color: rgba(255, 255, 255, 0.9);
+    margin-bottom: 10;
+    padding-horizontal: 10;
+    font-family: Avenir Next;
+    border: ${props => (props.error ? '1px solid tomato' : '1px solid #999999')}
+    border-radius: 4px;
+`;
