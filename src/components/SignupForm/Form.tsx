2d0ba79dbe9598e19fe876c06d9c5c0747c311d7
Johnny Phelan <phelanjo@oregonstate.edu>
2019-10-28 13:00:08 -0700

    Moved styled sign up components to own file, clean up imports in Sign Up Form

--- src/components/SignupForm/Form.tsx
+++ src/components/SignupForm/Form.tsx
@@ -1,42 +1,15 @@
 import React from 'react';
-import { ButtonText, Input, Logo } from '../basic';
+import { ButtonText, Logo } from '../basic';
+import { SignupContainer, SignupText, SignupView, SignupInput } from './signup_style';
 import { Button } from '../SignupButton';
-import styled from 'styled-components/native';
-
-export const SignupContainer = styled.KeyboardAvoidingView`
-    padding: 20px;
-    flex: 1;
-    justify-content: center;
-    background-color: #E9E9E9;
-`;
-
-export const SignupText = styled.Text`
-    margin-bottom: 10;
-    text-align: center;
-    font-family: Avenir Next;
-`;
-
-export const SignupView = styled.View`
-    padding: 20px;
-    border-radius: 5;
-    background-color: #E6B43C;
-`;
-
-export const SignupInput = styled.TextInput`
-    height: 40;
-    background-color: rgba(255, 255, 255, 0.9);
-    margin-bottom: 10;
-    padding-horizontal: 10;
-    font-family: Avenir Next;
-    border: ${props => (props.error ? '1px solid tomato' : '1px solid #999999')}
-    border-radius: 4px;
-`;
+import { FormLabel } from '../LoginForm/login_style';
 
 export default SignupComponent = props => (
     <SignupContainer behavior='padding'>
         <Logo source={require('../../assets/images/lt.png')} />
         <SignupText>Sign up for a new CBT Habit Tracker Account!</SignupText>
         <SignupView>
+            <FormLabel>Username</FormLabel>
             <SignupInput
                 placeholder="username"
                 placeholderTextColor='#666'
@@ -47,6 +20,7 @@ export default SignupComponent = props => (
                 error={props.touched.username && props.errors.username}
                 onSubmitEditing={() => this.emailInput.focus()}
             />
+            <FormLabel>Email</FormLabel>
             <SignupInput
                 placeholder="email"
                 placeholderTextColor='#666'
@@ -60,6 +34,7 @@ export default SignupComponent = props => (
                 onSubmitEditing={() => this.passwordInput.focus()}
                 error={props.touched.email && props.errors.email}
             />
+            <FormLabel>Password</FormLabel>
             <SignupInput
                 placeholder="password"
                 placeholderTextColor='#666'
---
+++ src/components/SignupForm/signup_style.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native';
+
+export const SignupContainer = styled.KeyboardAvoidingView`
+    padding: 20px;
+    flex: 1;
+    justify-content: center;
+    background-color: #E9E9E9;
+`;
+
+export const SignupText = styled.Text`
+    margin-bottom: 10;
+    text-align: center;
+    font-family: Avenir Next;
+`;
+
+export const SignupView = styled.View`
+    padding: 20px;
+    border-radius: 5;
+    background-color: #E6B43C;
+`;
+
+export const SignupInput = styled.TextInput`
+    height: 40;
+    background-color: rgba(255, 255, 255, 0.9);
+    margin-bottom: 10;
+    padding-horizontal: 10;
+    font-family: Avenir Next;
+    border: ${props => (props.error ? '1px solid tomato' : '1px solid #999999')}
+    border-radius: 4px;
+`;
