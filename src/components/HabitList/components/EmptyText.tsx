f4e4725c3cbf23550011b7ecb0ca80daabbe1411
Johnny Phelan <phelanjo@oregonstate.edu>
2019-11-01 12:44:33 -0700

    Move Habit List styles into own file, clean up imports.

--- src/components/HabitList/components/EmptyText.tsx
+++ src/components/HabitList/components/EmptyText.tsx
@@ -1,12 +1,4 @@
 import React from 'react';
-import styled from 'styled-components/native'
-
-const EmptyText = styled.Text`
-    font-family: Avenir Next;
-    font-size: 20px;
-    align-self: center;
-    font-weight: bold;
-    margin-top: 12px;
-`;
+import { EmptyText } from '../habit_list_styles';
 
 export default () => <EmptyText> Add a new habit below! </EmptyText>
