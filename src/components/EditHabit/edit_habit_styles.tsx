49aa5a3bffb37cec82d742a97ba6ab2b3ca92d56
Johnny Phelan <phelanjo@oregonstate.edu>
2019-11-01 12:15:54 -0700

    export styled component.

--- src/components/EditHabit/edit_habit_styles.tsx
+++ src/components/EditHabit/edit_habit_styles.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-const Container = styled.KeyboardAvoidingView`
+export const Container = styled.KeyboardAvoidingView`
     padding: 20px;
     background-color: #ffffff;
 `;
