b6ebb23e457eeae0234ccbc6de9d8598765a8871
carlosgonzalez96 <carlosglez817@gmail.com>
2019-11-04 10:41:06 -0800

    Added new file for steak image component

---
+++ src/components/StreakImage.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    ImageBackground
+} from 'react-native';
+
+import {
+    Container, View
+} from 'native-base';
+
+import Constants from 'expo-constants';
+
+import {
+    Streak
+} from '../components'
+
+export default class SteakImage extends React.Component {
+    //TODO
+}
