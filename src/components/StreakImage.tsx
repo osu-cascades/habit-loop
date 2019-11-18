aee7b7f82a5f09b406672708566fcda6cc933913
carlosgonzalez96 <carlosglez817@gmail.com>
2019-11-18 12:40:31 -0800

    Reverted some styles back for create button for the moment

--- src/components/StreakImage.tsx
+++ src/components/StreakImage.tsx
@@ -1,41 +1,10 @@
 import React, { Component } from 'react';
-// import {
-//     Text,
-//     ImageBackground
-// } from 'react-native';
-
-// import {
-//     View
-// } from 'native-base';
 
 import {
     Streak
-} from '../components'
+} from '../components';
 
-import { View, FlameImgBg, Text } from './styling/StreakImageStyles2'
-import styled from 'styled-components'
-//dont use this one
-//import { styles } from './styling/StreakImageStyles'
-
-// const StreakImage = () => {
-//     return (
-//         // <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-//         <View style={styles.viewContainer}>
-//             <ImageBackground
-//                 source={require('../assets/images/cbtFlameIcon-sml.png')}
-//                 style={styles.flameIconImage}
-//             >
-//                 <Text
-//                     adjustsFontSizeToFit={true}
-//                     numberOfLines={1}
-//                     style={styles.streakNum}
-//                 >
-//                     <Streak />
-//                 </Text>
-//             </ImageBackground>
-//         </View>
-//     );
-// }
+import { View, FlameImgBg, Text } from './styling/StreakImageStyles2';
 
 const StreakImage = () => {
     return (
