527cf94d44dfb4b50dd55008b77bb65a9be4e69f
carlosgonzalez96 <carlosglez817@gmail.com>
2019-11-22 11:15:51 -0800

    Removed native-base import sinnce it is not getting used

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { Settings, SignoutButton } from '../components';
 import { Text } from 'react-native';
 import Constants from 'expo-constants';
-import {
-  View
-} from 'native-base';
-
 import { Container } from '../components/styling/SettinngsScreenStyles';
 import { from } from 'zen-observable';
 
