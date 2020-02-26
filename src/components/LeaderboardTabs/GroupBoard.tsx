6c1c8668a1458ebfac9108df3092b90d2addd83f
Johnny Phelan <phelanjo@oregonstate.edu>
2020-02-26 10:39:41 -0800

    Update styles for group leaderboards

--- src/components/LeaderboardTabs/GroupBoard.tsx
+++ src/components/LeaderboardTabs/GroupBoard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, ListItem, Badge } from 'native-base';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Group } from 'react-native';
 import { useQuery, gql } from '@apollo/client';
 
 import { Loading } from '@src/components';
@@ -20,6 +20,7 @@ export const GroupBoard = ({ itemId }) => {
     variables: { item_id: itemId },
   });
 
+  console.log(data);
   if (loading) return <Loading />;
   if (error) return <Text>`Error loading data! ${error.message}`</Text>;
 
@@ -39,8 +40,8 @@ export const GroupBoard = ({ itemId }) => {
           <Badge style={styles.badge}>
             <Text>{row.index + 1}</Text>
           </Badge>
-          <Text style={styles.listItemText}>{row.item.username}</Text>
-          <Badge>
+          <Text>{row.item.username}</Text>
+          <Badge style={styles.streakBadge}>
             <Text>Streak {row.item.score}</Text>
           </Badge>
         </ListItem>
@@ -62,11 +63,12 @@ const styles = StyleSheet.create({
     borderRightWidth: 5,
   },
   badge: {
-    backgroundColor: '#F78E2A',
+    backgroundColor: '#E6B43C',
     marginRight: '5%',
   },
-  listItemText: {
-    alignSelf: 'flex-start',
+  streakBadge: {
+    backgroundColor: '#E6B43C',
+    marginLeft: '15%',
   },
 });
 
