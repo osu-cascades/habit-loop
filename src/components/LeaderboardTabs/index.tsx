ca00cf1ac01d52cde1f28968e5479e1e94367363
carlosgonzalez96 <carlosglez817@gmail.com>
2020-03-02 10:24:49 -0800

    Recolor Leaderboards tabs

--- src/components/LeaderboardTabs/index.tsx
+++ src/components/LeaderboardTabs/index.tsx
@@ -7,10 +7,10 @@ export default class LeaderboardTabs extends Component {
   render() {
     return (
       <Tabs tabBarUnderlineStyle={{ backgroundColor: '#E6B43C' }}>
-        <Tab heading="Top 25" activeTextStyle={{ color: '#000' }}>
+        <Tab heading="Top 25" tabStyle={{ backgroundColor: '#fff' }} activeTextStyle={{ color: '#000' }}>
           <TopStreakList />
         </Tab>
-        <Tab heading="Groups" activeTextStyle={{ color: '#000' }}>
+        <Tab heading="Groups" tabStyle={{ backgroundColor: '#fff' }} activeTextStyle={{ color: '#000' }}>
           <GroupList />
         </Tab>
       </Tabs>
