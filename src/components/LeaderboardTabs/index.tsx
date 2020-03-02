a7bc2f798cb9a15d4e3cfe9e6c6a6d7d1ff4d70a
carlosgonzalez96 <carlosglez817@gmail.com>
2020-03-02 10:17:57 -0800

    Recolor Leaderboards tabs

--- src/components/LeaderboardTabs/index.tsx
+++ src/components/LeaderboardTabs/index.tsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { Tab, Tabs } from 'native-base';
-import TopStreakList  from './TopStreakList';
+import TopStreakList from './TopStreakList';
 import GroupList from './GroupList';
 
 export default class LeaderboardTabs extends Component {
   render() {
     return (
-        <Tabs>
-          <Tab heading="Top 25">
-            <TopStreakList />
-          </Tab>
-          <Tab heading="Groups">
-            <GroupList />
-          </Tab>
-        </Tabs>
+      <Tabs tabBarUnderlineStyle={{ backgroundColor: '#E6B43C' }}>
+        <Tab heading="Top 25" activeTextStyle={{ color: '#000' }}>
+          <TopStreakList />
+        </Tab>
+        <Tab heading="Groups" activeTextStyle={{ color: '#000' }}>
+          <GroupList />
+        </Tab>
+      </Tabs>
     );
   }
 }
