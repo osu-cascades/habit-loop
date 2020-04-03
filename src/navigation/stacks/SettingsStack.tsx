a918ebee4188316ec634582dfe408256d83fbb0e
Johnny Phelan <phelanjo@oregonstate.edu>
2020-04-03 14:43:33 -0700

    Add "Promote User" Screen to Settings navigation stack.

--- src/navigation/stacks/SettingsStack.tsx
+++ src/navigation/stacks/SettingsStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SettingsScreen } from '@src/navigation/screens';
+import { SettingsScreen, PromoteUserScreen } from '@src/navigation/screens';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -8,6 +8,7 @@ export const SettingsStack = () => {
   return (
     <Navigator>
       <Screen name="Settings" component={SettingsScreen} />
+      <Screen name="PromoteUser" component={PromoteUserScreen} />
     </Navigator>
   );
 };
