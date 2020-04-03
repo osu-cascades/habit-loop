8694aa8db81814ebf210eab3b263a00e54823172
Johnny Phelan <phelanjo@oregonstate.edu>
2020-04-03 14:44:08 -0700

    Add "Promote User" screen to navigation.

---
+++ src/navigation/screens/PromoteUserScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PromoteForm } from '@src/components';
+
+export const PromoteUserScreen = () => {
+  return <PromoteForm />;
+};
+
+PromoteUserScreen.navigationOptions = {
+  title: 'Promote User',
+};
