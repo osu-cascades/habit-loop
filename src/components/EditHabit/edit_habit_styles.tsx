0e8ec3ad1bbbcf30d7cd92a8573b5476b9cf18b3
carlosgonzalez96 <carlosglez817@gmail.com>
2020-04-24 13:20:22 -0700

    Add style component sto edit habit screen and change position of title

--- src/components/EditHabit/edit_habit_styles.tsx
+++ src/components/EditHabit/edit_habit_styles.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
 import styled from 'styled-components/native';
 
-export const Container = styled.KeyboardAvoidingView`
+export const ContainerContent = styled.KeyboardAvoidingView`
     padding: 20px;
+    background-color: #ffffff;
+    height: 100%;
+`;
+
+export const Container = styled.KeyboardAvoidingView`
     background-color: #ffffff;
     height: 100%;
 `;
@@ -14,4 +18,17 @@ export const UpdateButton = styled.TouchableOpacity`
   margin: 0 auto;
   margin-top: 10;
   border-radius: 100;
+`;
+
+export const Title = styled.Text`
+  text-align: center;
+  font-weight: bold;
+  font-size: 15;
+`;
+
+export const TitleContainer = styled.View`
+  border-bottom-width: 4;
+  borderColor: #E6B43C;
+  borderStyle: solid;
+  padding: 13px;
 `;
