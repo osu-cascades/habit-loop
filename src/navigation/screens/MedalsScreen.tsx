0d020e2d3b468a413ee540c71952172d435c48d9
Johnny Phelan <phelanjo@oregonstate.edu>
2020-05-08 11:06:43 -0700

    Create Medals screen and navigation stack

---
+++ src/navigation/screens/MedalsScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from '../../components/basic';
+
+export const MedalsScreen = () => {
+  return (
+    <Container>
+      <Text>Under Construction...</Text>
+    </Container>
+  );
+};
+
+MedalsScreen.navigationOptions = {
+  title: 'Medals',
+};
