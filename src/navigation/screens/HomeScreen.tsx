cf33d6a7ca698e88dfca849defcd96fc23a4f4cd
carlosgonzalez96 <carlosglez817@gmail.com>
2020-05-08 12:26:01 -0700

    Add scrollview to make page scrollable

--- src/navigation/screens/HomeScreen.tsx
+++ src/navigation/screens/HomeScreen.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Container } from 'native-base';
 import Constants from 'expo-constants';
+import { ScrollView } from 'react-native'
 import { PushNotifications, SignoutButton, PriorityHabit, StreakImage } from '@src/components';
 import { CreateGroupButton, JoinGroupButton } from '@src/components/Groups';
 import Groups from '../../components/LeaderboardTabs';
-import { ButtonContainer, HomeContainer } from '../../components/styling/HomeStyles';
+import { ButtonContainer, Ccontainer, HomeContainer } from '../../components/styling/HomeStyles';
 
 export const HomeScreen = () => {
   return (
     <HomeContainer>
-      <StreakImage />
-      <ButtonContainer>
-        <CreateGroupButton />
-        <JoinGroupButton />
-      </ButtonContainer>
-      <Groups />
-      {/* <PriorityHabit /> */}
-      <PushNotifications />
+      <ScrollView>
+        <StreakImage />
+        <ButtonContainer>
+          <CreateGroupButton />
+          <JoinGroupButton />
+        </ButtonContainer>
+        <Ccontainer>
+          <Groups />
+        </Ccontainer>
+        {/* <PriorityHabit /> */}
+        <PushNotifications />
+      </ScrollView>
     </HomeContainer>
   );
 };
